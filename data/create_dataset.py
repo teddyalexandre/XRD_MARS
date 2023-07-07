945ee87b398f6bc2e7a4605ab9f62a2f2b998641
bellacheanass <bel_ans@hotmail.com>
2023-07-07 16:35:52 +0200

    refactoring and pulling xrd from materials project

---
+++ data/create_dataset.py
@@ -0,0 +1,22 @@
+from mp_api.client import MPRester
+from utils import process_material
+from joblib import Parallel, delayed
+import pandas as pd
+
+
+# Initialize the MPRester
+with MPRester(api_key="bo70Q5XVKyZdImV77bFXHO2cDKdvVQ6F") as mpr:
+
+    # Initialize an empty list to store the data
+    data = []
+
+    # Fetch the list of material IDs
+    material_docs = mpr.summary.search(num_chunks=1)
+
+    # Use joblib to process the materials in parallel
+    data = Parallel(n_jobs=-1)(delayed(process_material)(material_doc.material_id, "bo70Q5XVKyZdImV77bFXHO2cDKdvVQ6F", 0.1, 0.1, "MoKa") for material_doc in material_docs)
+
+    # Save the dataset to a file
+    df = pd.DataFrame(data)
+    print(df.head())
+    df.to_csv("dataset.csv")
--- data/utils.py
+++ data/utils.py
@@ -39,11 +39,14 @@ def calculate_xrd_pattern(material_id, api_key, alpha, gamma, wavelength):
     return norm_signal, steps
 
 
-if __name__ == "__main__":
-    norm_signal, steps = calculate_xrd_pattern("mp-2680", "bo70Q5XVKyZdImV77bFXHO2cDKdvVQ6F", 0.1, 0.1, "MoKa")
-    plt.plot(steps, norm_signal)
-    # Set the plot title and axis labels
-    plt.title('XRD Pattern')
-    plt.xlabel('2 theta')
-    plt.ylabel('Intensity')
-    plt.show()
+def process_material(material_id, api_key, alpha, gamma, wavelength):
+    # Calculate the XRD pattern
+    norm_signal = calculate_xrd_pattern(material_id, api_key, alpha, gamma, wavelength)
+
+    # Fetch the space group
+    with MPRester(api_key=api_key) as mpr:
+        structure = mpr.get_structure_by_material_id(material_id)
+    sga = SpacegroupAnalyzer(structure)
+    space_group = sga.get_space_group_symbol()
+
+    return {"XRD Pattern": norm_signal, "Space Group": space_group}
