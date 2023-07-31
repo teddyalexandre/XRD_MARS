6cf758e7cd8bd464493bf2bb34f26ef653e6f4bc
bellacheanass <bel_ans@hotmail.com>
2023-07-31 13:52:06 +0200

    adding xrd calculator from cif files

--- data/utils.py
+++ data/utils.py
@@ -3,6 +3,7 @@ from mp_api.client import MPRester
 from pymatgen.analysis.diffraction.xrd import XRDCalculator
 from pymatgen.symmetry.analyzer import SpacegroupAnalyzer
 from scipy.special import wofz
+from pymatgen.core import Structure
 
 
 def V(x, alpha, gamma):
@@ -73,4 +74,49 @@ def process_material(material_id, api_key, alpha, gamma, wavelength):
         return {"XRD Pattern": norm_signal, "Space Group": space_group}
     except Exception as e:
         print(f"Error processing material {material_id}: {e}")
+        return {"XRD Pattern": None, "Space Group": None}
+
+
+def calculate_xrd_from_cif(cif_path, alpha, gamma, wavelength):
+    """
+    Calculate the X-ray diffraction (XRD) pattern for a structure from a CIF file and convolve it with a Voigt function.
+
+    Args:
+        cif_path (str): Path to the CIF file.
+        alpha (float): The Lorentzian component for the Voigt function.
+        gamma (float): The Gaussian component for the Voigt function.
+        wavelength (str): The type of radiation used for the diffraction. Common choices are 'CuKa' and 'MoKa'.
+
+    Returns:
+        dict: A dictionary with two keys: 'XRD Pattern' and 'Space Group'. The 'XRD Pattern' key corresponds to the
+              calculated XRD pattern convolved with a Voigt function. The 'Space Group' key corresponds to the space
+              group of the structure. If an error occurs during the calculation, both keys will have a value of None.
+    """
+    try:
+        structure = Structure.from_file(cif_path)
+
+        # Initialize an XRD calculator with a specific radiation type
+        calculator = XRDCalculator(wavelength=wavelength)
+
+        sga = SpacegroupAnalyzer(structure)
+        conventional_structure = sga.get_conventional_standard_structure()
+        space_group = sga.get_space_group_symbol()
+
+        # Calculate the XRD pattern
+        pattern = calculator.get_pattern(conventional_structure)
+
+        # Prepare the data
+        steps = np.linspace(min(pattern.x), max(pattern.x), num=10000)
+        norm_signal = np.zeros_like(steps)
+
+        # Replace each peak in the XRD pattern with a Voigt profile
+        for i in range(len(pattern.x)):
+            peak_position = pattern.x[i]
+            peak_intensity = pattern.y[i]
+            norm_signal += peak_intensity * V(steps - peak_position, alpha, gamma)
+
+        return {"XRD Pattern": (norm_signal, steps), "Space Group": space_group}
+
+    except Exception as e:
+        print(f"Error processing file {cif_path}: {e}")
         return {"XRD Pattern": None, "Space Group": None}
